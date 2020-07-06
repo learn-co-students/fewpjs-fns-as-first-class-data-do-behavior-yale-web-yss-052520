48a230d5c5b9dff8cbcc3a1366087bb40d131244
ZIming Mao <ziming.mao@yale.edu>
2020-07-06 17:13:00 -0400

    Done.

--- index.js
+++ index.js
@@ -10,4 +10,23 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+
+
+function greet(string){
+  const chars = string.split(":")
+  const hour = parseInt(chars[0], 10)
+  if (hour < 12){
+    return "Good Morning"
+  }
+  else if (hour > 17) {
+    return "Good Evening"
+  }
+  else {
+    return "Good Afternoon"
+  }
+}
 /* Write your implementation of displayMessage() */
+
+function displayMessage(string){
+  document.querySelector('#greeting').innerText = string
+}
