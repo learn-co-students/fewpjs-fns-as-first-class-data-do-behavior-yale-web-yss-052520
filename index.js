7969d5ffb8f49e157e46aa26405a854b3eb41399
adilgondal1 <adil.gondal@yale.edu>
2020-07-08 09:46:55 -0400

    Done.

--- index.js
+++ index.js
@@ -10,4 +10,18 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+function greet(time) {
+  const t = time.split(":")[0]
+  if (t < 12) {
+    return "Good Morning"
+  } else if (t < 17) {
+    return "Good Afternoon"
+  } else {
+    return "Good Evening"
+  }
+}
+
+function displayMessage(msg){
+  document.querySelector("h1").innerText = msg;
+}
 /* Write your implementation of displayMessage() */
