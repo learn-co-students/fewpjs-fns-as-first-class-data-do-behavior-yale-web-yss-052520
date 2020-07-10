fd32aaeb7dbccefb277c1e05b325b9db1ea3f7ec
zayzay500 <lindsay.pierce@yale.edu>
2020-07-09 22:09:06 -0400

    Done.

--- index.js
+++ index.js
@@ -10,4 +10,19 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+function greet(timeString) {
+  let time = timeString.split(':')
+  time = parseInt(time[0].concat(time[1]))
+  if (time < 1200) {
+    return "Good Morning"
+  } else if (time > 1700) {
+    return "Good Evening"
+  } else {
+    return "Good Afternoon"
+  }
+}
 /* Write your implementation of displayMessage() */
+function displayMessage(greeting) {
+  const node = document.querySelector('#greeting')
+  node.innerText = greeting
+}
